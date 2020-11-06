e310639c56caef87785718a59c10d328d2714f4f
Michał Maj <michalmaj116@gmail.com>
2020-11-06 11:18:16 +0100

    Split files and functions. Add grayscale, absolute_values and standardize arguments to VG and GI methods

--- R/gradient_x_input.R
+++ R/gradient_x_input.R
@@ -1,5 +1,5 @@
-#' Calculates vanilla gradient for a CNN.
-#' @description Calculates vanilla gradient for a CNN.
+#' Calculates gradientXinput for a CNN.
+#' @description Calculates gradientXinput for a CNN.
 #' @import tensorflow
 #' @param model Tensorflow model.
 #' @param input_imgs Input images if for of 4-D tensor.
@@ -8,17 +8,18 @@
 #' @param absolute_values Boolean. If `TRUE` absolute values of gradients will be returned.
 #' @param grayscale Boolean. Should gradients be converted from RGB to grayscale.
 #' @param standardize Boolean. Should gradients be standardized.
-#' @return Vanilla gradient for a CNN.
+#' @return gradientXinput for a CNN.
 #' @export
-vanilla_gradient <- function(model, input_imgs, preprocessing_function = NULL,
+gradient_x_input <- function(model, input_imgs, preprocessing_function = NULL,
                              class_index = NULL, absolute_values = TRUE,
                              grayscale = TRUE, standardize = TRUE) {
   if (!is.null(preprocessing_function)) {
     input_imgs <- preprocessing_function(input_imgs)
   }
   gradients <- get_input_output_gradients(model, input_imgs, class_index)
-  if (absolute_values) gradients <- tf$abs(gradients)
-  if (grayscale) gradients <- transform_rgb_images_to_grayscale(gradients)
-  if (standardize) gradients <- per_image_standardization(gradients)
-  gradients$numpy()
+  gradient_x_input <- tf$multiply(input_imgs, gradients)
+  if (absolute_values) gradient_x_input <- tf$abs(gradient_x_input)
+  if (grayscale) gradient_x_input <- transform_rgb_images_to_grayscale(gradient_x_input)
+  if (standardize) gradient_x_input <- per_image_standardization(gradient_x_input)
+  gradient_x_input$numpy()
 }
---
+++ R/utils.R
@@ -0,0 +1,44 @@
+#' Calculates gradient between input and output layers.
+#' @description Calculates gradient between input and output layers.
+#' @import keras
+#' @import tensorflow
+#' @importFrom magrittr %>%
+#' @param model Tensorflow model.
+#' @param input_imgs Input images if for of 4-D tensor.
+#' @param class_index Class index. If set to `NULL` index with max predicted probability will be selected.
+#' @return Gradient between input and output layers.
+#' @export
+get_input_output_gradients <- function(model, input_imgs, class_index = NULL) {
+  images <-  tf$cast(input_imgs, tf$float32)
+  if (is.null(class_index)) {
+    preds <- model$predict(images)
+    class_index <- which.max(preds)
+  }
+
+  with(tf$GradientTape() %as% t, {
+    t$watch(images)
+    preds <- model(images)
+    top_class <- preds[ , class_index]
+  })
+  t$gradient(top_class, images)
+}
+
+#' Transforms RGB images to grayscale.
+#' @description Transforms RGB images to grayscale.
+#' @import tensorflow
+#' @param rgb_images RGB images.
+#' @return Grayscale images.
+transform_rgb_images_to_grayscale <- function(rgb_images) {
+  tf$reduce_sum(rgb_images, axis = as.integer(-1))
+}
+
+#' Per image standardization.
+#' @description Per image standardization.
+#' @import tensorflow
+#' @param images Images.
+#' @return Standardized images.
+per_image_standardization <- function(images) {
+  tf$cast(
+    255 * tf$image$per_image_standardization(images), tf$uint8
+  )
+}
