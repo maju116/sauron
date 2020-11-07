fc8a0f30e321a92208b783aec6ab4b11ff0f19ca
Michał Maj <michalmaj116@gmail.com>
2020-11-07 15:27:57 +0100

    add smooth-grad and smooth-grad x inputs

--- R/smooth_grad_x_input.R
+++ R/smooth_grad_x_input.R
@@ -1,5 +1,5 @@
-#' Calculates smooth-grad for a CNN.
-#' @description Calculates smooth-grad for a CNN.
+#' Calculates smooth-gradXinput for a CNN.
+#' @description Calculates smooth-gradXinput for a CNN.
 #' @import tensorflow
 #' @param model Tensorflow model.
 #' @param input_imgs Input images if for of 4-D tensor.
@@ -10,15 +10,19 @@
 #' @param absolute_values Boolean. If `TRUE` absolute values of gradients will be returned.
 #' @param grayscale Boolean. Should gradients be converted from RGB to grayscale.
 #' @param standardize Boolean. Should gradients be standardized.
-#' @return smooth-grad for a CNN.
+#' @return smooth-gradXinput for a CNN.
 #' @export
-smooth_grad <- function(model, input_imgs, preprocessing_function = NULL,
+smooth_grad_x_input <- function(model, input_imgs, preprocessing_function = NULL,
                         class_index = NULL, num_samples = 5, noise_sd = 0.1,
                         absolute_values = TRUE, grayscale = TRUE, standardize = TRUE) {
   smooth_gradients <- calculate_smoothed_gradients(model, input_imgs, preprocessing_function,
                                                    class_index, num_samples)
-  if (absolute_values) smooth_gradients <- tf$abs(smooth_gradients)
-  if (grayscale) smooth_gradients <- transform_rgb_images_to_grayscale(smooth_gradients)
-  if (standardize) smooth_gradients <- per_image_standardization(smooth_gradients)
-  smooth_gradients$numpy()
+  if (!is.null(preprocessing_function)) {
+    input_imgs <- preprocessing_function(input_imgs)
+  }
+  smooth_gradients_x_input <- tf$multiply(input_imgs, smooth_gradients)
+  if (absolute_values) smooth_gradients_x_input <- tf$abs(smooth_gradients_x_input)
+  if (grayscale) smooth_gradients_x_input <- transform_rgb_images_to_grayscale(smooth_gradients_x_input)
+  if (standardize) smooth_gradients_x_input <- per_image_standardization(smooth_gradients_x_input)
+  smooth_gradients_x_input$numpy()
 }
