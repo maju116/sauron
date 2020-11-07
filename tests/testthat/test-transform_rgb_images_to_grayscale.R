eaca9c1487d8738ce806dbda1de1f767263f0527
Michał Maj <michalmaj116@gmail.com>
2020-11-07 20:25:00 +0100

    fix tests

--- tests/testthat/test-transform_rgb_images_to_grayscale.R
+++ tests/testthat/test-transform_rgb_images_to_grayscale.R
@@ -4,6 +4,7 @@ test_that("transform_rgb_images_to_grayscale transforms RGB images to grayscale"
   test_input <- array(sample(0:255, 2*4*4*3, replace = TRUE), dim = c(2, 4, 4, 3))
   test_output <- transform_rgb_images_to_grayscale(test_input)$numpy()
   expected_output <- rowSums(test_input, dims = 3)
+  dim(expected_output) <- c(dim(expected_output), 1)
 
   expect_equal(test_output, expected_output)
 })
