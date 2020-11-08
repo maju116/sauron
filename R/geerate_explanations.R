f8f0f39bee69ab7267b22c12f84e24c76243b47a
Michał Maj <michalmaj116@gmail.com>
2020-11-08 11:47:31 +0100

    automatic target_size calculation form model.

--- R/geerate_explanations.R
+++ R/geerate_explanations.R
@@ -9,7 +9,6 @@ available_methods <- c("V", "GI", "SG", "SGI", "IG")
 #' @importFrom purrr map
 #' @param model Tensorflow model.
 #' @param input_imgs_paths Input images paths.
-#' @param target_size Input images target size.
 #' @param preprocessing_function Preprocessing function. Default to `NULL`.
 #' @param class_index Class index. If set to `NULL` index with max predicted probability will be selected.
 #' @param methods Methods to be calculated.
@@ -18,15 +17,15 @@ available_methods <- c("V", "GI", "SG", "SGI", "IG")
 #' @param steps Integration steps. Must be positive integer.
 #' @param absolute_values Boolean. If `TRUE` absolute values of gradients will be returned.
 #' @param grayscale Boolean. Should gradients be converted from RGB to grayscale.
-#' @param standardize Boolean. Should gradients be standardized.
 #' @return Explanations for images.
 #' @export
-generate_explanations <- function(model, input_imgs_paths, target_size = c(299, 299),
+generate_explanations <- function(model, input_imgs_paths,
                               preprocessing_function = NULL,
                               class_index = NULL, methods = available_methods,
                               num_samples = 5, noise_sd = 0.1, steps = 20,
                               absolute_values = TRUE,
-                              grayscale = TRUE, standardize = TRUE) {
+                              grayscale = TRUE) {
+  target_size <- unlist(model$input$get_shape()$as_list())[1:2]
   input_imgs <- input_imgs_paths %>% map(~ {
     image_load(., target_size = target_size) %>%
       image_to_array() %>% array_reshape(dim = c(1, target_size, 3))
@@ -36,22 +35,22 @@ generate_explanations <- function(model, input_imgs_paths, target_size = c(299, 
   for (m in methods) {
     if (m == "V") {
       explanations[[m]] <- vanilla_gradient(model, input_imgs, preprocessing_function,
-                                          class_index, absolute_values, grayscale, standardize)
+                                          class_index, absolute_values, grayscale, TRUE)
     } else if (m == "GI") {
       explanations[[m]] <- gradient_x_input(model, input_imgs, preprocessing_function,
-                                          class_index, absolute_values, grayscale, standardize)
+                                          class_index, absolute_values, grayscale, TRUE)
     } else if (m == "SG") {
       explanations[[m]] <- smooth_grad(model, input_imgs, preprocessing_function,
                                      class_index, num_samples, noise_sd,
-                                     absolute_values, grayscale, standardize)
+                                     absolute_values, grayscale, TRUE)
     } else if (m == "SGI") {
       explanations[[m]] <- smooth_grad(model, input_imgs, preprocessing_function,
                                      class_index, num_samples, noise_sd,
-                                     absolute_values, grayscale, standardize)
+                                     absolute_values, grayscale, TRUE)
     } else if (m == "IG") {
       explanations[[m]] <- integrated_gradients(model, input_imgs, preprocessing_function,
                                               class_index, steps,
-                                              absolute_values, grayscale, standardize)
+                                              absolute_values, grayscale, TRUE)
     }
   }
   explanations
